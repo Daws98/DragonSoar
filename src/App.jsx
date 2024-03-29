ea40a380081e45783bac6cd050eaa9fab6987024
Dawson <125399990+Daws98@users.noreply.github.com>
2024-03-28 21:30:27 -0600

    proper formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './AppNavigator';
+import AppNavigator from './layouts/AppNavigator';
 
 const App = () => {
   return (
